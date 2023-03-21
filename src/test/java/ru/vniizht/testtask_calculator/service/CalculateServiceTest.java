1b7f66fd593c4dcbc4100ef17ee72c80e0c5296b
Ekyd <Ekyd777@gmail.com>
2023-03-21 23:11:46 +0300

    add tests

--- src/test/java/ru/vniizht/testtask_calculator/service/CalculateServiceTest.java
+++ src/test/java/ru/vniizht/testtask_calculator/service/CalculateServiceTest.java
@@ -1,6 +1,8 @@
 package ru.vniizht.testtask_calculator.service;
 
-import org.junit.Before;
+
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import ru.vniizht.testtask_calculator.model.OperationModel;
 
@@ -8,36 +10,56 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculateServiceTest {
 
-    protected OperationModel operationModel = new OperationModel("1+2*(3+4/2-4(1+2))*2+1");
-    protected CalculateService calculateService;
+    static OperationModel operationModel = new OperationModel();
+    static CalculateService calculateService = new CalculateService();
+    private final static String TEST_NUMBER = "7";
+    private final static String TEST_EXPRESSION = "1+2*(3+4/2-(1+2))*2+1";
 
-    public CalculateServiceTest(CalculateService calculateService) {
-        this.calculateService = calculateService;
-    }
 
-    @Before
-    public void setUp(){
-        calculateService.setTokens(operationModel.getExpression());
+    @BeforeAll
+    public static void setUp() {
+        operationModel.setExpression(TEST_NUMBER);
         calculateService.setPos(0);
+
     }
+
     @Test
     public void eval() {
-        assertEquals(10, Integer.parseInt(calculateService.eval()));
+        prepareEval();
+        Double evalNumber = Double.parseDouble(calculateService.eval());
+        assertEquals(10, evalNumber);
+    }
+
+    private void prepareEval(){
+        operationModel.setExpression(TEST_EXPRESSION);
+        calculateService.setTokens(operationModel.getExpression());
     }
 
     @Test
     public void fibonacci() {
+        int fibonacci = calculateService.fibonacci(operationModel);
+        assertEquals(13, fibonacci);
     }
 
     @Test
     public void factorial() {
+        Long factorial = calculateService.factorial(operationModel);
+        assertEquals(5040, factorial);
     }
 
     @Test
     public void sqrt() {
+        operationModel.setExpression(TEST_NUMBER);
+        calculateService.setPos(0);
+        Double sqrt = calculateService.sqrt(operationModel);
+        assertEquals((Double) Math.sqrt(7), sqrt);
     }
 
     @Test
     public void power() {
+        operationModel.setExpression(TEST_NUMBER);
+        calculateService.setPos(0);
+        Double power = calculateService.power(operationModel);
+        assertEquals((Double) Math.pow(7, 2), power);
     }
 }
